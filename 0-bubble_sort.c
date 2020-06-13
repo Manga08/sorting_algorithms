0adea801930d4a6e73e010207a9c47630a89a036
fernando <sandovbarr@gmail.com>
2020-06-12 19:14:30 -0500

    [ADD] files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - that sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: pointer to array
+ * @size: size
+ * 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
