e67a9afc1275620e8f772712e3745cceadf3fdf9
fernando <sandovbarr@gmail.com>
2020-06-13 19:49:07 -0500

    [ADD]Task

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - that sorts a doubly linked list
+ * of integers in ascending order using the
+ * Insertion sort algorithm
+ * @list: lis ot sort
+ * Return: None
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *aux = *list, *handler = aux;
+
+        if (!*list)
+                return;
+        while (aux)
+        {
+                handler = aux;
+                while(handler->prev && (handler->prev->n > handler->n))
+                {
+                        handler->prev->next = handler->next;
+                        if (handler->next)
+                                handler->next->prev = handler->prev;
+                        handler->next = handler->prev;
+                        handler->prev = handler->next->prev;
+                        handler->next->prev = handler;
+                        if (handler->prev)
+                                handler->prev->next = handler;
+                        else
+                                *list = handler;
+                        print_list(*list);
+                }
+                aux = aux->next;
+        }
+}
